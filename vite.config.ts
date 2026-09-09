02a2e9baf31cfe2fb12895a812e8ee12741051de
david <jdchavezr917@gmail.com>
2026-09-09 08:38:03 -0600

    pwa 2

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
       },
       workbox: {
         navigateFallback: '/',
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
       },
     }),
